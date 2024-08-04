bdad08280789585146d68c303be7b20f4b878b65
RevazPeradze <my.reziko@gmail.com>
2024-08-04 16:22:17 +0400

    Add files via upload

---
+++ level 06/classwork/classwork.py
@@ -0,0 +1,8 @@
+name = input()
+print(f'hello your name is {name}') 
+price = 12.5
+height = 183
+aura = -10000
+user_name = input('write your name')
+user_lastname = input('write your last name')
+print(f'System welcomes you {user_name} {user_lastname} ')
---
+++ level 06/homework/homework.py
@@ -0,0 +1,10 @@
+name = input()
+print(f'hello your name is {name}') 
+price = 12.5
+height = 183
+aura = -10000
+user_name = input('write your name')
+user_lastname = input('write your last name')
+print(f'System welcomes you {user_name} {user_lastname} ')
+#input არის ისეთი მოწყობილობები რომლებითაც ინფორმაცია კომპიუტერში შედის მაგ:კლავიატურა
+#output არის მოწყობილობები რომბლებითაც ინფორმაცია კომპიუტერიდან გამოდის მაგ:მონიტორი
